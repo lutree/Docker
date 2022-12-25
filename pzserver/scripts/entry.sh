1e00c9c1146bce966aa32c9be5aee41dd0257a74
DESKTOP\lutre <lutree@outlook.kr>
2022-12-26 01:26:45 +0900

    zomboid 설정 대거 수정

---
+++ pzserver/scripts/entry.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+cd ${STEAMAPPDIR}
+
+######################################
+#                                    #
+# Process the arguments in variables #
+#                                    #
+######################################
+ARGS=""
+
+# Set the server memory. Units are accepted (1024m=1Gig, 2048m=2Gig, 4096m=4Gig): Example: 1024m
+if [ -n "${MEMORY}" ]; then
+  ARGS="${ARGS} -Xmx${MEMORY} -Xms${MEMORY}"
+fi
+
+# Option to perform a Soft Reset
+if [ "${SOFTRESET}" == "1" ] || [ "${SOFTRESET,,}" == "true" ]; then
+  ARGS="${ARGS} -Dsoftreset"
+fi
+
+# End of Java arguments
+ARGS="${ARGS} -- "
+
+# Disables Steam integration on server.
+# - Default: Enabled
+if [ "${NOSTEAM}" == "1" ] || [ "${NOSTEAM,,}" == "true" ]; then
+  ARGS="${ARGS} -nosteam"
+fi
+
+# Sets the path for the game data cache dir.
+# - Default: ~/Zomboid
+# - Example: /server/Zomboid/data
+if [ -n "${CACHEDIR}" ]; then
+  ARGS="${ARGS} -cachedir=${CACHEDIR}"
+fi
+
+# Launches the game in debug mode.
+# - Default: Disabled
+if [ "${DEBUG}" == "1" ] || [ "${DEBUG,,}" == "true" ]; then
+  ARGS="${ARGS} -debug"
+fi
+
+# Option to bypasses the enter-a-password prompt when creating a server.
+# This option is mandatory the first startup or will be asked in console and startup will fail.
+# Once is launched and data is created, then can be removed without problem.
+# Is recommended to remove it, because the server logs the arguments in clear text, so Admin password will be sent to log in every startup.
+if [ -n "${ADMINPASSWORD}" ]; then
+  ARGS="${ARGS} -adminpassword ${ADMINPASSWORD}"
+fi
+
+# You can choose a different servername by using this option when starting the server.
+if [ -n "${SERVERNAME}" ]; then
+  ARGS="${ARGS} -servername ${SERVERNAME}"
+else
+  # If not servername is set, use the default name in the next step
+  SERVERNAME="servertest"
+fi
+
+# If preset is set, then the config file is generated when it doesn't exists or SERVERPRESETREPLACE is set to True.
+if [ -n "${SERVERPRESET}" ]; then
+  # If preset file doesn't exists then show an error and exit
+  if [ ! -f "${STEAMAPPDIR}/media/lua/shared/Sandbox/${SERVERPRESET}.lua" ]; then
+    echo "*** ERROR: the preset ${SERVERPRESET} doesn't exists. Please fix the configuration before start the server ***"
+    exit 1
+  # If SandboxVars files doesn't exists or replace is true, copy the file
+  elif [ ! -f "${HOMEDIR}/Zomboid/Server/${SERVERNAME}_SandboxVars.lua" ] || [ "${SERVERPRESETREPLACE,,}" == "true" ]; then
+    echo "*** INFO: New server will be created using the preset ${SERVERPRESET} ***"
+    echo "*** Copying preset file from \"${STEAMAPPDIR}/media/lua/shared/Sandbox/${SERVERPRESET}.lua\" to \"${HOMEDIR}/Zomboid/Server/${SERVERNAME}_SandboxVars.lua\" ***"
+    mkdir -p "${HOMEDIR}/Zomboid/Server/"
+    cp -nf "${STEAMAPPDIR}/media/lua/shared/Sandbox/${SERVERPRESET}.lua" "${HOMEDIR}/Zomboid/Server/${SERVERNAME}_SandboxVars.lua"
+    sed -i "1s/return.*/SandboxVars = \{/" "${HOMEDIR}/Zomboid/Server/${SERVERNAME}_SandboxVars.lua"
+    # Remove carriage return
+    dos2unix "${HOMEDIR}/Zomboid/Server/${SERVERNAME}_SandboxVars.lua"
+    # I have seen that the file is created in execution mode (755). Change the file mode for security reasons.
+    chmod 644 "${HOMEDIR}/Zomboid/Server/${SERVERNAME}_SandboxVars.lua"
+  fi
+fi
+
+# Option to handle multiple network cards. Example: 127.0.0.1
+if [ -n "${IP}" ]; then
+  ARGS="${ARGS} ${IP} -ip ${IP}"
+fi
+
+# Set the DefaultPort for the server. Example: 16261
+if [ -n "${PORT}" ]; then
+  ARGS="${ARGS} -port ${PORT}"
+fi
+
+# Option to enable/disable VAC on Steam servers. On the server command-line use -steamvac true/false. In the server's INI file, use STEAMVAC=true/false.
+if [ -n "${STEAMVAC}" ]; then
+  ARGS="${ARGS} -steamvac ${STEAMVAC,,}"
+fi
+
+# Steam servers require two additional ports to function (I'm guessing they are both UDP ports, but you may need TCP as well).
+# These are in addition to the DefaultPort= setting. These can be specified in two ways:
+#  - In the server's INI file as SteamPort1= and SteamPort2=.
+#  - Using STEAMPORT1 and STEAMPORT2 variables.
+if [ -n "${STEAMPORT1}" ]; then
+  ARGS="${ARGS} -steamport1 ${STEAMPORT1}"
+fi
+if [ -n "${STEAMPORT2}" ]; then
+  ARGS="${ARGS} -steamport2 ${STEAMPORT1}"
+fi
+
+# Fix to a bug in start-server.sh that causes to no preload a library:
+# ERROR: ld.so: object 'libjsig.so' from LD_PRELOAD cannot be preloaded (cannot open shared object file): ignored.
+export LD_LIBRARY_PATH="${STEAMAPPDIR}/jre64/lib:${LD_LIBRARY_PATH}"
+
+## Fix the permissions in the data and workshop folders
+chown -R 1000:1000 /home/steam/pz-dedicated/steamapps/workshop /home/steam/Zomboid
+
+#su - steam -c "export LD_LIBRARY_PATH=\"${STEAMAPPDIR}/jre64/lib:${LD_LIBRARY_PATH}\" && cd ${STEAMAPPDIR} && pwd && ./start-server.sh ${ARGS}"
+
+su - steam -c "${STEAMCMDDIR}/steamcmd.sh +force_install_dir \"${STEAMAPPDIR}\" \
+                                    +login anonymous \
+                                    +app_update \"${STEAMAPPID}\" validate \
+                                    +quit"
+
+su - steam -c "export LC_ALL=\"ko_KR.EUC-KR\" && \
+               rm -rf /home/steam/Zomboid/Server/* && \
+               git clone https://github.com/lutree/pzserver_config.git /home/steam/Zomboid/Server && \
+               "
+
+#if [ ! -d "${HOMEDIR}/Zomboid/Server/.git" ]; then
+#  su - steam -c "rm -rf /home/steam/Zomboid/Server/*"
+#  su - steam -c "git clone https://github.com/lutree/pzserver_config.git /home/steam/Zomboid/Server"
+#else
+#  cd /home/steam/Zomboid/Server
+#  su - steam -c "git fetch --all"
+#  su - steam -c "git reset --hard origin/master"
+#  su - steam -c "git pull origin/master"
+#fi
+
+su - steam -c "export LD_LIBRARY_PATH=\"${STEAMAPPDIR}/jre64/lib:${LD_LIBRARY_PATH}\" && \
+    export LC_ALL=\"ko_KR.EUC-KR\" && \
+    export LANG=\"ko_KR.EUC-KR\" && \
+    export LANGUAGE=\"ko_KR.EUC-KR\" && \
+    cd ${STEAMAPPDIR} && \
+    pwd && \
+    ./start-server.sh ${ARGS}"
